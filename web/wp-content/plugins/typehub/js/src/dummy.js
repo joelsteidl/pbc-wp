f95fe30a61d321c4cd55b9658c052bc78cdfc043
Joel Steidl <joel@atendesigngroup.com>
2023-05-10 22:09:39 -0600

    Removing unused themes and plugins

--- web/wp-content/plugins/typehub/js/src/dummy.js
+++
@@ -1,9 +0,0 @@
-/* 
-    shouldComponentUpdate(nextProps,nextState){
-
-    }
-
-     
-    props.clickedFont === item ? itemBG = {background:'#e3e3e3',padding:10 ,cursor:'pointer',fontWeight:500} : itemBG = {background:'#fff',padding:10,cursor:'pointer'}
-   
-*/
